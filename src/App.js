7a580cdbb948f61489c2e0f62673556f596f8056
Larissa Oliveira <oliveir5uwm@gmail.com>
2021-09-16 14:28:59 -0300

    feat: finalizado

--- src/App.js
+++ src/App.js
@@ -1,43 +1,46 @@
 import CardsList from "./components/cards-list";
-import useEffect from "react";
+import { useEffect, useState} from 'react'
 import "./App.css";
 
 export const App = () => {
-  const [showDeck, setShowDeck] = useState(false);
-  const [cardsList, setCardsList] = useState([]);
-  const [deck, setDeck] = useState("");
+const [showDeck, setShowDeck] = useState(false);
+const [cardsList, setCardsList] = useState([]);
+const [deck, setDeck] = useState("");
 
-  const handleDeckRequest = () => {
-    fetch("https://deckofcardsapi.com/api/deck/new/")
-      .then((res) => res.json())
-      .then((res) => console.log(res));
-  };
-
-  const handleCardsRequest = (deckId) => {
-    fetch("https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52")
-      .then((res) => res.json())
-      .then((res) => setCardsList([...res.cards]));
-  };
-
-  const handleShowDeck = () => {
-    setShowDeck(false);
-  };
-
-  useEffect(() => {
-    handleDeckRequest();
-  }, [deck]);
-
-  useEffect(() => {
-    if (deck) handleCardsRequest(deck);
-  }, []);
-
-  return (
-    <div className="main-container">
-      <h1 className="main-title">Debugue para ver o baralho</h1>
-      <button onChange={handleShowDeck} className="new-deck-button">
-        Novo baralho
-      </button>
-      {ShowDeck & <CardsList cardsList={cardsList} />}
-    </div>
-  );
+const handleDeckRequest = () => {
+fetch("https://deckofcardsapi.com/api/deck/new/")
+.then((res) => res.json())
+.then((res) => setDeck(res.deck_id))
+.catch((err) => console.log(err))
 };
+
+const handleCardsRequest = (deckId) => {
+fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=52`)
+.then((res) => (res).json())
+.then((res) => setCardsList(res.cards));
+};
+
+const handleShowDeck = () => {
+setShowDeck(true);
+};
+
+useEffect(() => {
+handleDeckRequest();
+}, []);
+
+useEffect(() => {
+if (deck !== '') {handleCardsRequest(deck)};
+}, [deck]);
+
+return (
+<div className="main-container">
+<h1 className="main-title">Debugue para ver o baralho</h1>
+<button onClick={handleShowDeck} className="new-deck-button">
+Novo baralho
+</button>
+{showDeck && <CardsList cardsList={cardsList} />}
+</div>
+);
+};
+
+export default App
